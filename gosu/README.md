c2c52f94a6d981d59e3e589ba943bbda5f03e7d0
Charles Moulliard <ch007m@gmail.com>
2021-06-21 10:14:22 +0200

    Refactor gosu scripts. Still failing as user 1000 cannot execute groupadd or useradd

--- gosu/README.md
+++ gosu/README.md
@@ -9,7 +9,7 @@ make build
 ### Run
 
 ```shell script
-docker run -u 1000 --rm -it quay.io/snowdrop/openjdk11-git /bin/bash
+docker run -u 1000 --rm -it snowdrop/openjdk11-git-gosu /bin/bash
 Current UID : 1000
 useradd: Permission denied.
 useradd: cannot lock /etc/passwd; try again later.
--- gosu/entrypoint.sh
+++ gosu/entrypoint.sh
@@ -2,16 +2,17 @@
 set -e
 
 # If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 1000 as default
-CURRENT_UID=${uid:-1000}
+USER_ID=${uid:-1000}
+GROUP_ID=${LOCAL_GROUP_ID:-$USER_ID}
 
-# Notify user about the UID selected
-echo "Current UID : $CURRENT_UID"
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+groupadd -g $GROUP_ID thegroup
 
 # Create user called "docker" with selected UID
-# useradd -m -d /home/$LOCAL_USER_NAME -u $CURRENT_UID $LOCAL_USER_NAME
-useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m $LOCAL_USER_NAME
+# useradd -m -d /home/$LOCAL_USER_NAME -u $USER_ID $LOCAL_USER_NAME
+useradd --shell /bin/bash -u $USER_ID -g thegroup -o -c "" -m $LOCAL_USER_NAME
 export HOME=/home/$LOCAL_USER_NAME
 export USER=$LOCAL_USER_NAME
 
 # Execute process
-exec gosu $LOCAL_USER_NAME "$@"
+exec gosu $LOCAL_USER_NAME:thegroup "$@"
