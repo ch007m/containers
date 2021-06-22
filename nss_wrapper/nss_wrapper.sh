de83ae6593c92839ce59c4f4b53c769a8bf417dc
Charles Moulliard <ch007m@gmail.com>
2021-06-22 21:31:05 +0200

    Refactor project to use https://github.com/atbentley/docker-nss-wrapper

--- nss_wrapper/nss_wrapper.sh
+++ nss_wrapper/nss_wrapper.sh
@@ -3,11 +3,7 @@
 # $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-
-OUT_DIR=${NSS_DIR:-/home/jboss}
-
+envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
 export LD_PRELOAD=libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=${OUT_DIR}/build.passwd
-export NSS_WRAPPER_GROUP="/etc/group"
 
-envsubst < /usr/local/share/passwd.template > ${OUT_DIR}/build.passwd
+exec $@
